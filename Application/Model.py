895abbbba9c51417f38e2ff23b859440122e710f
dbeezt <dbrereton1995@gmail.com>
2021-01-29 00:58:45 +0000

    Enabled .txt recording of model settings, logging, and grid composition of .gif frames.

--- Application/Model.py
+++ Application/Model.py
@@ -1,11 +1,13 @@
 import collections
+import functools
 import glob
 import math
-import networkx as nx
+import operator
 import os
 import PIL
 import random
 import time
+import networkx as nx
 from Agents import AgentPopulation
 from Graph import Graph
 from GraphPlot import GraphPlot
@@ -13,21 +15,39 @@ from DataPlot import DataPlot
 
 
 class Model:
-    def __init__(self, output_dir: str, options, log, mode: str = "automatic"):
+    def __init__(self, output_dir: str, options, log = '', mode: str = "automatic"):
         # reason for not putting datetime here would be to facilitate manual mode
+        # this is generated each time the ne model class is inst'd
+        # containts dir name, date, time
         self.output_dir = output_dir
-        self.graphml_export_path = f"{self.output_dir}/graphs"
-        self.plot_export_path = f"{self.output_dir}/plots"
-        self.stats_export_path = f"{self.output_dir}/stats"
+        self.log = log
+        # this records the time for cycle starts (i think)
         self.latest_run_time = time.strftime("%H:%M:%S")
-        self.condition_count_per_iteration = []
-        self.financial_impact_count_per_iteration = []
+        self.create_datetime_output_dir()
+        self.save_model_settings_to_txt(options)
         self.enforce_input_value_data_types(options)
-        # ref to gui
-        # self.print_to_log(log, 'test')
+    
+    def print_to_log(self, message):
+        if self.log:
+            self.log.insert('end', f"{message}\n")
 
-    # def print_to_log(self, log, message):
-    #     log.insert('end', f"{message}\n")
+    def create_datetime_output_dir(self):
+        os.makedirs(self.output_dir , exist_ok=True)
+   
+    def save_model_settings_to_txt(self, options):
+        option_list_names = ['Compound Settings', 'Pandemic Settings', 'Financial Settings']
+        compound_setting_names = ['Total Agents', 'No. Cycles', 'Graph Type', 'Random Seed']
+        pandemic_setting_names = ['Geographic Cohesion', 'No. Iterations', 'Ease of Transmission', 'Time to Recover']
+        financial_setting_names = ['Financial Cohesion', 'No. Iterations', 'Lockdown Severity', 'Loan Threshold']
+        all_setting_names = [compound_setting_names, pandemic_setting_names, financial_setting_names]
+        with open(f"{self.output_dir}/model_variables.txt", 'w') as txt_file:
+            txt_file.write("COMPOUND PANDEMIC/FINANCIAL MODEL\n")
+            txt_file.write("---------------------------------\n")
+            for option_list in range(0, len(option_list_names)):
+                txt_file.write(f"\n{option_list_names[option_list]}\n")
+                txt_file.write("-----------------\n\n")
+                for option in range(0, len(options[option_list])):
+                    txt_file.write(f"{all_setting_names[option_list][option]}: {options[option_list][option]}\n")
 
     def enforce_input_value_data_types(self, options):
         self.total_agents = int(options[0][0])
@@ -46,6 +66,13 @@ class Model:
         self.financial_loan_threshold = float(options[2][3])
 
     def auto_run(self):
+        txt_export_path = self.output_dir
+        graphml_export_path = f"{self.output_dir}/graphs"
+        plot_export_path = f"{self.output_dir}/plots"
+        stats_export_path = f"{self.output_dir}/stats"
+        condition_count_per_iteration = []
+        financial_impact_count_per_iteration = []
+        
         # if we persist the attribute values of node sizes, it means
         # either financial uses node (degree) sizes of pandemic of vice versa
         # would we rather see node size represented by geographic or financial significance?
@@ -53,39 +80,43 @@ class Model:
         infected_agents = 1
         agents = AgentPopulation(self.total_agents, infected_agents).agents
         pandemic = Graph(agents, self.pandemic_cohesion, self.graph_type, self.seed)
-        self.condition_count_per_iteration.append(
+        condition_count_per_iteration.append(
             self.sum_agent_attributes(pandemic.graph.nodes.data("condition"))
         )
         financial = Graph(
             agents, self.financial_cohesion, self.graph_type, self.seed + 1
         )
         financial.persist_attributes_between_graphs(pandemic.graph)
-        self.financial_impact_count_per_iteration.append(
+        financial_impact_count_per_iteration.append(
             self.sum_agent_attributes(financial.graph.nodes.data("financial_impact"))
         )
 
         node_positions = nx.spring_layout(pandemic.graph, k=0.5, seed=self.seed)
         GraphPlot(
-            title='PANDEMIC MODEL: Cycle 0.0',
+            title="PANDEMIC MODEL: Cycle 0.0",
             graph=pandemic,
             coords=node_positions,
-            export_path=self.concat_plot_write_path("network", "pandemic", 0, 0),
+            export_path=self.concat_plot_write_path(plot_export_path, "network", "pandemic", 0, 0),
         ).render_and_export_graph()
         GraphPlot(
-            title='FINANCIAL MODEL: Cycle 0.0',
-            graph=financial,
+            title="FINANCIAL MODEL: Cycle 0.0",
+            graph=financial, 
             coords=node_positions,
-            export_path=self.concat_plot_write_path("network", "financial", 0, 0),
+            export_path=self.concat_plot_write_path(plot_export_path, "network", "financial", 0, 0),
         ).render_and_export_graph()
         pandemic.compose_and_write_csv_of_graph_data(
-            alt_graph=financial.graph, output_path=self.concat_csv_write_path(0, 0)
+            alt_graph=financial.graph, output_path=self.concat_csv_write_path(stats_export_path, 0, 0)
         )
 
         for cycle in range(0, self.model_cycles):
             # Used for generating iterative seed when performing random interaction checks
             self.cycle = cycle
+            # formata these L's in prin to logs
+            self.print_to_log(f"    Cycle #{cycle + 1}")
             print(f"Cycle #{cycle + 1}")
             self.latest_run_time = time.strftime("%H:%M:%S")
+            # conditions_at_current_cycle = 
+            self.print_to_log(f"        {self.sum_agent_attributes(pandemic.graph.nodes.data('condition'))}")
             for iteration in range(0, self.pandemic_iterations):
                 print(f"  Pandemic Iteration #{cycle + 1}.{iteration + 1}")
                 pandemic.persist_attributes_between_graphs(financial.graph)
@@ -100,18 +131,18 @@ class Model:
                     graph=pandemic,
                     coords=node_positions,
                     export_path=self.concat_plot_write_path(
-                        "network", "pandemic", cycle + 1, iteration + 1
+                        plot_export_path, "network", "pandemic", cycle + 1, iteration + 1
                     ),
                 ).render_and_export_graph()
                 self.write_graph_to_graphml(
-                    pandemic.graph, "pandemic", cycle + 1, iteration + 1
+                    pandemic.graph, graphml_export_path, "pandemic", cycle + 1, iteration + 1
                 )
-            self.condition_count_per_iteration.append(
+            condition_count_per_iteration.append(
                 self.sum_agent_attributes(pandemic.graph.nodes.data("condition"))
             )
             for iteration in range(0, self.financial_iterations):
                 self.current_iteration = iteration + 1
-                print(f"  Financial Iteration #{cycle + 1}.{iteration + 1}")
+                print(f" Financial Iteration #{cycle + 1}.{iteration + 1}")
                 financial.persist_attributes_between_graphs(pandemic.graph)
                 financial.graph = self.run_financial_model(
                     financial.graph,
@@ -123,67 +154,90 @@ class Model:
                     graph=financial,
                     coords=node_positions,
                     export_path=self.concat_plot_write_path(
-                        "network", "financial", cycle + 1, iteration + 1
+                        plot_export_path, "network", "financial", cycle + 1, iteration + 1
                     ),
                 ).render_and_export_graph()
                 self.write_graph_to_graphml(
-                    financial.graph, "financial", cycle + 1, iteration + 1
+                    financial.graph, graphml_export_path, "financial", cycle + 1, iteration + 1
                 )
-                self.financial_impact_count_per_iteration.append(
+                financial_impact_count_per_iteration.append(
                     self.sum_agent_attributes(
                         financial.graph.nodes.data("financial_impact")
                     )
                 )
             pandemic.compose_and_write_csv_of_graph_data(
                 alt_graph=financial.graph,
-                output_path=self.concat_csv_write_path(cycle + 1, iteration + 1),
+                output_path=self.concat_csv_write_path(stats_export_path, cycle + 1, iteration + 1),
             )
             # f"{graphml_dir_path}/{self.latest_run_time}-{cycle}.{iteration}.graphml"
 
         self.latest_pandemic_gif = self.compose_gif_from_pngs(
-            f"{self.plot_export_path}/network/pandemic"
+            f"{plot_export_path}/network/pandemic"
         )
         self.latest_financial_gif = self.compose_gif_from_pngs(
-            f"{self.plot_export_path}/network/financial"
+            f"{plot_export_path}/network/financial"
         )
 
-        data_plot = DataPlot(
-            path_to_data=f"{self.stats_export_path}/graph/node_attributes",
-            export_path=f"{self.plot_export_path}/data",
-        ).create_plot()
+        # data_plot = DataPlot(
+        #     path_to_data=f"{stats_export_path}/graph/node_attributes",
+        #     export_path=f"{plot_export_path}/data",
+        # ).create_plot()
 
-    def concat_csv_write_path(self, cycle, iteration):
-        return f"{self.stats_export_path}/graph/node_attributes/{self.latest_run_time}-{cycle}.{iteration}.csv"
+    def concat_csv_write_path(self, stats_dir, cycle, iteration):
+        return f"{stats_dir}/graph/node_attributes/{self.latest_run_time}-{cycle}.{iteration}.csv"
 
-    def concat_plot_write_path(self, plot, model, cycle, iteration):
-        return f"{self.plot_export_path}/{plot}/{model}/{self.latest_run_time}-{cycle}.{iteration}.png"
+    def concat_plot_write_path(self, plot_dir, plot, model, cycle, iteration):
+        return f"{plot_dir}/{plot}/{model}/{self.latest_run_time}-{cycle}.{iteration}.png"
 
-    def write_graph_to_graphml(self, graph, model, cycle, iteration):
-        graphml_dir_path = f"{self.graphml_export_path}/{model}"
+    def write_graph_to_graphml(self, graph, graphml_dir, model, cycle, iteration):
+        graphml_dir_path = f"{graphml_dir}/{model}"
         os.makedirs(graphml_dir_path, exist_ok=True)
         graphml_file_path = (
             f"{graphml_dir_path}/{self.latest_run_time}-{cycle}.{iteration}.graphml"
         )
         nx.write_graphml(G=graph, path=graphml_file_path)
 
-    def compose_gif_from_pngs(self, path_to_images: str):
-        files_in = f"{path_to_images}/*.png"
-        file_out = f"{path_to_images}.gif"
 
-        img, *imgs = [PIL.Image.open(file) for file in sorted(glob.glob(files_in))]
-        img.save(
-            fp=file_out,
+    def compose_gif_from_pngs(self, path_to_images: str):
+        def compose_image_grid_from_frames(images):
+            columns = 3
+            rows = (math.ceil(len(images)/columns))
+            width, height = images[0].size
+            grid = PIL.Image.new('RGB', size = ((columns * width), (rows * height)), color='#FFFFFF')
+            grid_width, grid_height = grid.size
+
+            for x, image in enumerate(images):
+                box = (((x % columns) * width), ((x // columns) * height))
+                grid.paste(image, box = box)
+            return grid
+        
+        frames_in = f"{path_to_images}/*.png"
+        gif_out = f"{path_to_images}/model.gif"
+
+        frame_zero, *frames = [PIL.Image.open(file) for file in sorted(glob.glob(frames_in))]
+        frame_zero.save(
+            fp=gif_out,
             format="GIF",
-            append_images=imgs,
+            append_images=frames,
             save_all=True,
-            duration=400,
+            duration=600,
             loop=0,
         )
-        return file_out
+
+        grid_out = f"{path_to_images}/grid.png"
+        grid = compose_image_grid_from_frames(images = [frame_zero, *frames])
+        grid.save(
+            fp=grid_out,
+            format='PNG'
+        )
+
+        return gif_out
+
 
     def sum_agent_attributes(self, attribute):
-        condition_counter = collections.Counter([y for (x, y) in attribute])
-        return condition_counter
+        counter = collections.Counter([y for (x, y) in attribute])
+        return (sorted(counter.items(), key=operator.itemgetter(0)))
+
 
     def run_pandemic_model(self, graph, transmission_rate, time_to_recover):
         def update_edge_fill_colour(condition: str):
@@ -193,7 +247,6 @@ class Model:
                 "removed": "#d3d3d3",
             }.get(condition, "#CCCCCC")
 
-        transmission_rate, time_to_recover = transmission_rate, time_to_recover
         time_before_becoming_contagious = math.ceil(time_to_recover / 5)
         # print(time_before_becoming_contagious)
         agent_population: int = len(graph.nodes)
@@ -207,6 +260,7 @@ class Model:
                     )
                     graph.nodes[source_node]["condition"] = "removed"
                     graph.nodes[source_node]["able_to_recover"] = False
+                    # should add blank return here?
                 if graph.nodes[source_node]["condition"] == "infectious":
                     # print(graph.nodes[source_node]["time_exposed"])
                     # print(graph.nodes[agent]["able_to_recover"])
@@ -245,7 +299,6 @@ class Model:
         return graph
 
     def run_financial_model(self, graph, lockdown_severity, loan_threshold):
-        lockdown_severity, loan_threshold = lockdown_severity, loan_threshold
         agent_population = len(graph.nodes)
         for agent in range(0, agent_population):
             if graph.nodes[agent]["financial_impact"] != "bust":
@@ -295,13 +348,13 @@ class Model:
                                     graph.nodes[source_node][
                                         "current_asset_value"
                                     ] += source_node_bail_out_amount
+                        graph.nodes[source_node]["financial_impact"] = (
+                            graph.nodes[source_node]["current_asset_value"]
+                            / graph.nodes[source_node]["initial_asset_value"]
+                        )
+
                         if graph.nodes[source_node]["financial_impact"] == "bust":
                             print(f"Node {source_node} is bust")
-                        else:
-                            graph.nodes[source_node]["financial_impact"] = (
-                                graph.nodes[source_node]["current_asset_value"]
-                                / graph.nodes[source_node]["initial_asset_value"]
-                            )
                             # graph.nodes[source_node][
                             #     "financial_impact"
                             # ] = self.update_financial_status(financial_damage)
